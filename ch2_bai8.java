5ce12f8ac501b699830e66e7e2a5a32bf1989d30
BuiMinhThuc <buiminhthucvv2002@gmail.com>
2022-03-02 15:52:36 +0700

    first commit

---
+++ ch2_bai8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ch2_bai8{
+   public static void main(String[] args) {
+    int n, sum = 0, so;
+    float avg;
+     Scanner scanner = new Scanner(System.in); {
+        System.out.print("Nhap vao so cac so nguyen: ");
+        n=scanner.nextInt();
+        for(int i=1;i<=n;i++){
+            System.out.println("Nhap vao so thu "+i+": ");
+            so=scanner.nextInt();
+            sum+=so;
+        }
+    }
+    avg=(float) sum/n;
+    System.out.println("Trung binh cong cua "+n+" so tren la: "+avg );
+   }
+}
